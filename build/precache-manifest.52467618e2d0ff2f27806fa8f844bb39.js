0612f4d041880bdc6292d0d1e70eeac284326a77
Monikka <monikka.ravichandran@gmail.com>
2020-10-01 18:09:12 -0400

    Adding the build files

---
+++ build/precache-manifest.52467618e2d0ff2f27806fa8f844bb39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57da1e6e0c4c10ef32bca45966e48b3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f93928312a5c580135a",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "a0d335bacf3b15e41c51",
+    "url": "/static/css/main.5c1a2c2c.chunk.css"
+  },
+  {
+    "revision": "7f93928312a5c580135a",
+    "url": "/static/js/2.5d0833b1.chunk.js"
+  },
+  {
+    "revision": "fee3ef41109770b3c68abe9f8e3cdf99",
+    "url": "/static/js/2.5d0833b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0d335bacf3b15e41c51",
+    "url": "/static/js/main.637a96eb.chunk.js"
+  },
+  {
+    "revision": "b9dbe97be3884a56cfe9",
+    "url": "/static/js/runtime-main.eba039cb.js"
+  },
+  {
+    "revision": "406add80862db91e937179febe1fe1cc",
+    "url": "/static/media/ceirs-logo.406add80.png"
+  }
+]);
